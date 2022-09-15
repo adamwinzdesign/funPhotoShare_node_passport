6ae0b550ba8cf77f5c4b3fbd50d5f211862bc056
adamwinzdesign <adamwinzdesign@gmail.com>
2022-09-15 15:41:05 -0500

    updated auth-router to include login route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -29,6 +29,33 @@ router.post("/register", (req, res) => {
 		});
 });
 
+// log in
+router.post("/login", (req, res) => {
+	const { email, password } = req.body;
+
+	Users.findBy({ email })
+		.first()
+		.then((user) => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = generateToken(user);
+
+				res.status(200).json({
+					message: `Welcome back, ${user.username}!`,
+					id: user.id,
+					username: user.username,
+					token,
+					status: user.status,
+					location: user.location,
+				});
+			} else {
+				res.status(401).json({ message: "Invalid credentials!" });
+			}
+		})
+		.catch((error) => {
+			res.status(500).json({ message: "Error logging in!", error });
+		});
+});
+
 function generateToken(user) {
 	const payload = {
 		username: user.username,
