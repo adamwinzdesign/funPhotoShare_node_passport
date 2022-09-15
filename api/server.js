6682fe9678c18e0bc2515f1ce6c78a333c2bd5e9
adamwinzdesign <adamwinzdesign@gmail.com>
2022-09-15 15:04:00 -0500

    updated server.js

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const authRouter = require("../auth/auth-router");
+
 const server = express();
 
 server.use(helmet());
@@ -10,6 +12,8 @@ server.use(morgan("common"));
 server.use(cors());
 server.use(express.json({ limit: "50mb" }));
 server.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
 	res.json({ server: "Up and running! Let's make a dope API!  Start testing!" });
